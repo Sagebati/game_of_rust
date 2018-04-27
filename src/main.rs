9f0f27e4292b5b854a6e68449e8e35f78e1ea4f4
Samuel Batissou <samuelbatissou@gmail.com>
2018-04-27 16:38:43 +0200

    almost2

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,82 @@
 extern crate sdl2;
+extern crate num;
+extern crate rand;
 
-const HAUTEUR: u16 = 64;
-const LARGEUR: u16 = 64;
-const SIZE: usize = (HAUTEUR * LARGEUR) as usize;
+use sdl2::pixels;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
 
-struct board_t([bool; SIZE]);
+use sdl2::gfx::primitives::DrawRenderer;
+
+use rand::Rng;
+
+const HAUTEUR: usize = 800;
+const LARGEUR: usize = 600;
+const SIZE: usize = (HAUTEUR * LARGEUR);
+
+
+type BoardT = [bool; SIZE];
 
 fn main() {
-    let mut board: board_t = board_t([false; SIZE]);
+    let mut board: BoardT = [false; SIZE];
 
-    let ctx = sdl2::init().unwrap();
-    let video_ctx = ctx.video().unwrap();
-    let mut timer = ctx.timer().unwrap();
+    randomise(board);
 
-    // Create a window
 
-    let mut window = match video_ctx.window("eg01", 640, 480).position_centered().opengl().build() {
-        Ok(window) => window,
-        Err(err) => panic!("failed to create window: {}", err)
-    };
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsys = sdl_context.video().unwrap();
+    let window = video_subsys.window("rust-sdl2_gfx: draw line & FPSManager", LARGEUR as u32, HAUTEUR as u32)
+        .position_centered()
+        .opengl()
+        .build()
+        .unwrap();
 
-    // Display the window for 3 seconds
+    let mut canvas = window.into_canvas().build().unwrap();
 
-    window.show();
-    timer.delay(3000);
+
+    canvas.set_draw_color(pixels::Color::RGB(0, 0, 0));
+    canvas.clear();
+    canvas.present();
+
+    let mut lastx = 0;
+    let mut lasty = 0;
+
+    let mut events = sdl_context.event_pump().unwrap();
+
+    'main: loop {
+        for event in events.poll_iter() {
+            match event {
+                Event::Quit { .. } => break 'main,
+
+                Event::KeyDown { keycode: Some(keycode), .. } => {
+                    if keycode == Keycode::Escape {
+                        break 'main;
+                    } else if keycode == Keycode::Space {
+                        println!("space down");
+                        canvas.set_draw_color(pixels::Color::RGB(0, 0, 0));
+                        canvas.clear();
+
+                        canvas.set_draw_color(pixels::Color::RGB(255, 255, 255));
+                        for i in 0..SIZE {
+                            if board[i] == true {
+                                let (x, y) = get_2d_from_1d(i);
+                                canvas.draw_point(sdl2::rect::Point::new(x,y));
+                            }
+                        }
+                        board = cal_next_board(board);
+
+                        canvas.present();
+                    }
+                }
+                _ => {}
+            }
+        }
+    }
 }
 
 
-fn cal_next_board(board: board_t) -> board_t {
-    let res = [false; SIZE];
+fn cal_next_board(board: BoardT) -> BoardT {
+    let mut res = [false; SIZE];
     for i in 0..SIZE {
         let neighbours = get_neigbours(board, i);
         let cc = count_bit(neighbours);
@@ -43,14 +91,14 @@ fn cal_next_board(board: board_t) -> board_t {
         }
     }
 
-    return board_t(res);
+    return res;
 }
 
 
 fn count_bit(arr: [bool; 8]) -> u8 {
     let mut count = 0;
-    for b in arr {
-        if b == 0 {
+    for b in arr.iter() {
+        if *b == true {
             count += 1
         }
     }
@@ -58,21 +106,22 @@ fn count_bit(arr: [bool; 8]) -> u8 {
 }
 
 
-fn get(board: board_t, x: u16, y: u16) -> bool {
-    if LARGEUR * x + y < SIZE {
+fn get(board: BoardT, x: usize, y: usize) -> bool {
+    let test = (LARGEUR * x + y) as usize;
+    if test < SIZE {
         return board[LARGEUR * x + y];
     } else {
         return false;
     }
 }
 
-fn set(board: board_t, x: u16, y: u16, val: bool) {
+fn set(mut board: BoardT, x: usize, y: usize, val: bool) {
     board[LARGEUR * x + y] = val
 }
 
 
-fn get_neigbours(board: board_t, i: usize) -> [bool; 8] {
-    let res = [false; 8];
+fn get_neigbours(board: BoardT, i: usize) -> [bool; 8] {
+    let mut res = [false; 8];
     let x = i / LARGEUR;
     let y = i % LARGEUR;
     res[0] = get(board, x, y + 1);
@@ -86,3 +135,15 @@ fn get_neigbours(board: board_t, i: usize) -> [bool; 8] {
 
     return res;
 }
+
+fn get_2d_from_1d(i: usize) -> (i32, i32) {
+    return ((i / LARGEUR) as i32, (i % LARGEUR) as i32);
+}
+
+
+fn randomise(mut board: BoardT) {
+    for i in 0..SIZE {
+        board[i] = if rand::thread_rng().gen_range(0, 2) == 1 { true } else { false };
+    }
+}
+
